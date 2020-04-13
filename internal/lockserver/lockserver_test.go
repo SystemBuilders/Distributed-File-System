99892b96217ccb322eaceec5a786c9dde239ecb4
Suraj Singh <suraj1998@gmail.com>
2020-04-13 13:38:36 +0530

    added additional test

--- internal/lockserver/lockserver_test.go
+++ internal/lockserver/lockserver_test.go
@@ -9,8 +9,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSingleLockAndRelease(t *testing.T) {
-	go StartServer()
+func TestDoubleLockAndRelease(t *testing.T) {
+	t.Logf("acquire a release a acquire a release a")
+	// go StartServer()
 	assert := assert.New(t)
 	client, err := rpc.DialHTTP("tcp", "localhost:55550")
 	if err != nil {
@@ -29,4 +30,49 @@ func TestSingleLockAndRelease(t *testing.T) {
 	if err != nil {
 		assert.Fail(fmt.Sprintf("Failed to release lock: %v", err))
 	}
+
+	err = client.Call("Service.Acquire", "1", &y)
+
+	if err != nil {
+		assert.Fail(fmt.Sprintf("Failed to acquire lock: %v", err))
+	}
+	err = client.Call("Service.Release", "1", &y)
+	if err != nil {
+		assert.Fail(fmt.Sprintf("Failed to release lock: %v", err))
+	}
+
+	t.Logf("PASSED")
+
+}
+
+func TestMultipleLockAndRelease(t *testing.T) {
+	t.Logf("acquire a acquire b release b release a\n")
+	assert := assert.New(t)
+	client, err := rpc.DialHTTP("tcp", "localhost:55550")
+
+	var y float32
+	y = 3.4
+	if err != nil {
+		log.Fatal("Connection error: ", err)
+	}
+	err = client.Call("Service.Acquire", "1", &y)
+
+	if err != nil {
+		assert.Fail(fmt.Sprintf("Failed to acquire lock: %v", err))
+	}
+	err = client.Call("Service.Acquire", "2", &y)
+	if err != nil {
+		assert.Fail(fmt.Sprintf("Failed to release lock: %v", err))
+	}
+
+	err = client.Call("Service.Release", "2", &y)
+	if err != nil {
+		assert.Fail(fmt.Sprintf("Failed to release lock: %v", err))
+	}
+
+	err = client.Call("Service.Release", "1", &y)
+	if err != nil {
+		assert.Fail(fmt.Sprintf("Failed to release lock: %v", err))
+	}
+
 }
