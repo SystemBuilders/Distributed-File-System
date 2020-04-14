bcebd4b3bb31ae4bd017e8ddfe2496dcb3c1ac71
SUMUKHA-PK <sumukhapk46@gmail.com>
2020-04-14 13:17:37 +0530

    added a timer to ensure server starts

--- internal/lockserver/lockserver_test.go
+++ internal/lockserver/lockserver_test.go
@@ -5,12 +5,15 @@ import (
 	"log"
 	"net/rpc"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSingleLockAndRelease(t *testing.T) {
 	go StartServer()
+	timer1 := time.NewTimer(5 * time.Millisecond)
+	<-timer1.C
 	assert := assert.New(t)
 	client, err := rpc.DialHTTP("tcp", "localhost:55550")
 	if err != nil {
@@ -25,7 +28,9 @@ func TestSingleLockAndRelease(t *testing.T) {
 	if err != nil {
 		assert.Fail(fmt.Sprintf("Failed to acquire lock: %v", err))
 	}
+
 	err = client.Call("Service.Release", "1", &y)
+
 	if err != nil {
 		assert.Fail(fmt.Sprintf("Failed to release lock: %v", err))
 	}
